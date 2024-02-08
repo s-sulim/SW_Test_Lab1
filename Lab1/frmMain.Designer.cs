476a1d84116250129eae95d5c9a43896739c00e7
Svetozar Sulim <svetozarniks@gmail.com>
2024-02-08 08:43:03 +0100

    Add project files.

---
+++ Lab1/frmMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Lab1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.lbdOutput = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(100, 182);
+            this.tbA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(73, 30);
+            this.tbA.TabIndex = 1;
+            this.tbA.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            this.tbA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress);
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(348, 419);
+            this.tbB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(73, 30);
+            this.tbB.TabIndex = 2;
+            this.tbB.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            this.tbB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress);
+            // 
+            // tbC
+            // 
+            this.tbC.Location = new System.Drawing.Point(504, 182);
+            this.tbC.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(73, 30);
+            this.tbC.TabIndex = 3;
+            this.tbC.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            this.tbC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbA);
+            this.panel1.Controls.Add(this.tbB);
+            this.panel1.Controls.Add(this.tbC);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(605, 490);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lab1.Properties.Resources.triangle__1_;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 478);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(634, 322);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(125, 40);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calcuate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(634, 107);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(203, 184);
+            this.tbOutput.TabIndex = 6;
+            // 
+            // lbdOutput
+            // 
+            this.lbdOutput.AutoSize = true;
+            this.lbdOutput.Location = new System.Drawing.Point(640, 72);
+            this.lbdOutput.Name = "lbdOutput";
+            this.lbdOutput.Size = new System.Drawing.Size(74, 23);
+            this.lbdOutput.TabIndex = 7;
+            this.lbdOutput.Text = "Output: ";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(900, 535);
+            this.Controls.Add(this.lbdOutput);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Triangle type calc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label lbdOutput;
+    }
+}
+
---
+++ Lab1/frmMain.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            CalculateTriangle();
+        }
+        private string Check4Isosceles(List<int> triangleSides)
+        {
+            bool flg = false;
+            string res = "";
+            foreach (var t in triangleSides)
+            {
+                var tmp = new List<int>(triangleSides);
+                tmp.Remove(t);
+                foreach (var tt in tmp)
+                {
+                    if (tt == t)
+                    {
+                        res = "Рівнобедрений." + Environment.NewLine + Environment.NewLine + $"Серед сторін даного трикутника є дві, що дорівнюють {tt}.";
+                        break;
+                    }
+                }
+                if (flg)
+                {
+                    break;
+                }
+            }
+            return res;
+        }
+        private string Check4RightOrObtuse(List<int> triangleSides)
+        {
+            string res = "";
+            int max = triangleSides.Max();
+            var tmp = new List<int>(triangleSides);
+            tmp.Remove(max);
+            double sum = 0;
+            foreach (var item in tmp)
+            {
+                sum += Math.Pow(item, 2);
+            }
+            if (Math.Pow(max, 2) == sum)
+            {
+                res = "Прямокутний." + Environment.NewLine + Environment.NewLine + $"Сума квадратів катетів дорівнює квадрату гіпотенузи, тобто сторони з довжиною {max}.";
+            }
+            else if (Math.Pow(max, 2) > sum)
+            {
+                res = "Тупокутний." + Environment.NewLine + Environment.NewLine + $"Квадрат сторони з довжиною {max} є більшим за суму квадратів інших двох сторін.";
+            }
+            return res;
+        }
+        private string Check4Equilateral(List<int> triangleSides)
+        {
+            string res = "";
+            double avg = triangleSides.Average();
+            if (avg == triangleSides.First())
+            {
+                res = "Рівносторонній." + Environment.NewLine + Environment.NewLine + $"Усі сторони трикутника дорівнюють {triangleSides.First()}.";
+            }
+            return res;
+        }
+        private string Check4Oxygon(List<int> triangleSides)
+        {
+            string res = "";
+            foreach (var t in triangleSides)
+            {
+                var tmp = new List<int>(triangleSides);
+                tmp.Remove(t);
+                var sum = 0d;
+                foreach (var tt in tmp)
+                {
+                    sum += Math.Pow(tt, 2);
+                }
+                if (Math.Pow(t, 2) < sum)
+                {
+                    res = "Гострокутний." + Environment.NewLine + Environment.NewLine + $"Квадрат кожної сторони менший за суму квадратів інших двох сторін.";
+                }
+                else
+                {
+                    res = "";
+                }
+            }
+            return res;
+        }
+        private void CalculateTriangle()
+        {
+            tbOutput.Text = "";
+            var triangleSides = new List<int>() { Convert.ToInt32(tbA.Text) , Convert.ToInt32(tbB.Text), Convert.ToInt32(tbC.Text) };
+            
+            string res = Check4Equilateral(triangleSides); //перевіряємо на рівносторонність
+            if (!String.IsNullOrEmpty(res))
+            {
+                tbOutput.Text = res;
+            }
+            else
+            {
+                res = Check4Isosceles(triangleSides); //перевіряємо на рівнобедреність
+                if (!String.IsNullOrEmpty(res))
+                {
+                    tbOutput.Text = res;
+                }
+                else
+                {
+                    res = Check4RightOrObtuse(triangleSides);  //перевіряємо на прямокутність або тупокутність
+                    if (!String.IsNullOrEmpty(res))
+                    {
+                        tbOutput.Text = res;
+                    }
+                    else
+                    {
+                        res = Check4Oxygon(triangleSides); //перевіряємо на гострокутність
+                        if (!String.IsNullOrEmpty(res))
+                        {
+                            tbOutput.Text = res;
+                        }
+                    }
+                }
+            }
+        }
+        private void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            if (((TextBox)sender).Text == "0" || String.Concat(((TextBox)sender).Text.GroupBy(c => c).SelectMany(g => $"{g.Key}")) == "0")
+            {
+                MessageBox.Show("A side of the triangle cannot be 0!", "Error",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ((TextBox)sender).Text = "";
+            }
+        }
+    }
+}
